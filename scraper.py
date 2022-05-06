b4be177600977860e28b18fb4408d11920f16ea9
s-301-13 <s-301-13@gumiforest.uek.krakow.pl>
2022-05-06 11:12:03 +0200

    06.05.2022 ASZ

--- scraper.py
+++ scraper.py
@@ -1,57 +1,53 @@
+import py_compile
+import selectors
 import requests
 from bs4 import BeautifulSoup 
 import json 
 
-url = "https://www.ceneo.pl/63490289#tab=reviews"  
+def get_item(ancestor, selector,attribute=None, return_list = False): 
+     try: 
+        if list: 
+            return [item.get_text().strip() for item in ancestor.select(selector)] 
+        if attribute: 
+             return ancestor.select_one(selector)[attribute]
+        return ancestor.select_one(selector).get_text().strip()
+     except (AttributeError, TypeError):  
+         return None 
+     
+    selectors = {
+                "author" : ["span.user-post__author-name"],
+                "recommendation" : ["span.user-post__author-recomendation >  em"],
+                "stars" : ["span.user-post__score-count"], 
+                "content" : ["div.user-post__text"], 
+                "useful" : ["button.vote-yes > span"], 
+                "useless" : ["button.vote-no > span"], 
+                "publish_date" : ["span.user-post__published > time:nth-child(1)","datetime"], 
+                "purchase_date" : ["span.user-post__published > time:nth-child(2)","datetime"] , 
+                "pros" : ["div[class$=\"positives\"]~ div.review-feature__item",None, True], 
+                "cons" : ["div[class$=\"negatives\"]~ div.review-feature__item", None, True]
+            }  
+
+link = input("Enter code product: ")
+url = f"https://www.ceneo.pl/{link}#tab=reviews"  
 all_opinions = []
 
 while(url): 
     response = requests.get(url) 
-
     page = BeautifulSoup(response.text, 'html.parser')
-
     opinions = page.select("div.js_product-review")   
-        for opinion in opinions:
-            opinion = opinions.pop(0)  
-            opinion_id = opinion["data-entry-id"] 
-            author = opinion.select_one("span.user-post__author-name").get_text().strip() 
-            try:
-                recommendation= page.select_one("span.user-post__author-recomendation >  em").get_text().strip() 
-            except AttributeError: 
-                recommendation = None 
-            stars = page.select_one("span.user-post__score-count").get_text().strip()
-            content = opinion.select_one("div.user-post__text").get_text().strip()
-            useful = opinion.select_one("button.vote-yes > span").get_text().strip()
-            useless = opinion.select_one("button.vote-no > span").get_text().strip()
-            publish_date = opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"] 
-            try:
-                purchase_date = opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"] 
-            except TypeError: 
-                purchase_date = None  
-
-            pros = opinion.select("div[class$=\"positives\"]~ div.review-feature__item") 
-            pros = [item.get_text().strip() for item in pros]  
-            cons = opinion.select("div[class$=\"negatives\"]~ div.review-feature__item") 
-            cons = [item.get_text().strip() for item in cons]
-
+        for opinion in opinions: 
+            
             single_opinion = {
-                "opinion_id": opinion,
-                "author" : author, 
-                "recommendation" : recommendation, 
-                "stars" : stars, 
-                "content" : content , 
-                "useful" : useful, 
-                "useless" : useless, 
-                "publish_date" : publish_date, 
-                "purchase_date" : purchase_date, 
-                "pros" : pros, 
-                "cons" : cons
-            }  
+                key:get_item(opinion, *value) 
+                for key, value in selectors.items()
+            }
+            single_opinion["opinion_id"] = opinion["data-entry-id"]
             all_opinions.append(single_opinion) 
     try:
-        url = "https://www.ceneo.pl"+page.select_one ("a.pagination__next")["href"]
+        url = "https://www.ceneo.pl"+ get_item(page,"a.pagination__next","href")
     except TypeError: 
         url = None 
 
-with open ("opinions/634902892.json","w", encoding = "UTF-8") as jf: 
+with open ("opinions/{link}.
+json","w", encoding = "UTF-8") as jf: 
     json.dump(all_opinions, jf , indent= 4, ensure_ascii = False )
