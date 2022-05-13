b1f5b8d73e1e0bbe1fd12495e119b36d9da3a15a
AleksandraSzoldra <s221398@student.uek.krakow.pl>
2022-05-13 11:18:36 +0200

    2022-05-13 ASz

---
+++ analyzer.py
@@ -0,0 +1,41 @@
+import os 
+import pandas as pd 
+import numpy as np
+from matplotlib import pyplot as plt
+
+print (*[filename.split(".")[0] for filename in os.listdir("./opinions")], sep="\n")
+product_id = input("Podaj indentyfikator produktu: ") 
+
+opinions = pd.read_json(f"opinions/{product_id}.json")
+print(opinions) 
+
+opinions.stars = opinions.stars.map(lambda x: float(x.split("/")[0].replace(",",".")))
+opinions_count = len(opinions.index)
+pros_count = opinions.pros.map(bool).sum()
+cons_count = opinions.cons.map(bool).sum() 
+average_score = opinions.stars.mean().round(2)
+
+
+recommendation = opinions.recommendation.value_counts(dropna = False).sort_index().reindex(["Nie polecam","Polecam",None])
+print(recommendation)
+recommendation.plot.pie(
+    label="", 
+    autopct = "%1.1f%%", 
+    colors = ["crimson","lightskyblue","forestgreen"]
+    labels = ["Nie polecam", "Polecam","Nie mam zdania"]
+    ) 
+plt.title("Rekomendacja") 
+plt.savefig(f"plots/{product_id}_recommendation.png")
+plt.close() 
+
+stars.opinions.stars.value_counts().sort_index().reindex(list(np.arange(0,5.5, 0.5)), fill_value = 0)
+stars.plot.bar()
+plt.title("Oceny produktu")
+plt.xlabel("Liczba gwiazdek") 
+plt.ylabel("Liczba opinii") 
+plt.grid(True)
+plt.xticks(rotation =0)
+plt.close()
+plt.savefig(f"plots/{product_id}_stars.png")
+plt.show()
+ #tabela przestawna w pandas -pd
